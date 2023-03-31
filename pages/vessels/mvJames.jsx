5b5746df2f5807fc1989671e37b00bcf57ccfc51
striker <goodluckodemilin@gmail.com>
2023-03-31 01:37:29 +0100

    text in verssle

---
+++ pages/vessels/mvJames.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MvJames() {
+  return <div>mr james</div>;
+}
---
+++ pages/vessels/mvThreeFevers.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function mvThreeFevers() {
+  return <div>ths cloud</div>;
+}
