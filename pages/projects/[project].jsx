0b051153930c0b2f06eb30f1937be0b35676bfee
striker <goodluckodemilin@gmail.com>
2023-04-01 01:59:21 +0100

    [project]* template

--- pages/projects/[project].jsx
+++ pages/projects/[project].jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { projects } from "@/utilitys/data";
+
 export async function getStaticPaths() {
   const paths = projects.map((item) => {
     return { params: { project: item.id.toString() } };
@@ -15,6 +17,7 @@ export async function getStaticPaths() {
 export async function getStaticProps(context) {
   const id = context.params.project;
   const data = projects[id];
+
   return {
     props: { data },
   };
@@ -22,6 +25,32 @@ export async function getStaticProps(context) {
 
 export default function Project({ data }) {
   console.log(data);
-  const id = useRouter.query;
-  return <div>{data.id}</div>;
+
+  const router = useRouter();
+  const currentIndex = projects.findIndex(
+    (item) => item.id === parseInt(router.query.project)
+  );
+  const prevIndex = currentIndex > 0 ? currentIndex - 1 : null;
+  const nextIndex =
+    currentIndex < projects.length - 1 ? currentIndex + 1 : null;
+
+  return (
+    <div>
+      <p>{data.id}</p>
+      <h1>{data.title}</h1>
+      <p>{data.description}</p>
+
+      {prevIndex !== null && (
+        <Link href={`/projects/${projects[prevIndex].id}`}>
+          <button>Previous project</button>
+        </Link>
+      )}
+
+      {nextIndex !== null && (
+        <Link href={`/projects/${projects[nextIndex].id}`}>
+          <button>Next project</button>
+        </Link>
+      )}
+    </div>
+  );
 }
