b0b6f98193885c0efe89e55f9328a125c22289f5
striker <goodluckodemilin@gmail.com>
2023-04-03 21:44:11 +0100

    REMOVED EVENT

--- pages/events.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { navheader } from "@/redux/action";
-import styles from "@/styles/events.module.css";
-export default function Events() {
-  const dispatch = useDispatch();
-  dispatch(navheader("event"));
-  return <div>this is events</div>;
-}
